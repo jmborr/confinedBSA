7ca350851c3afb5d152740d33974ad3b55353ae7
Jose Borreguero <borreguero@gmail.com>
2017-07-10 09:52:36 -0400

    Refs #1 rename certain directories and new h017_q0.90_ovl2.5

--- simulation/confinedBSA/h017_OSIq0.55_ovl1.5/equil_1.submitnext.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ];
-then
-  echo "need to pass a number as argument"
-  exit 1
-fi
-sed -e "s/_PREV_/$1/g" equil_1.extend.tpl > equil_1.extend.pbs
-qsub equil_1.extend.pbs
-
--- simulation/silica/cristobalite/confineBSA/poretop/yesbonds/topology_silica.py
+++ simulation/silica/cristobalite/confineBSA/poretop/yesbonds/topology_silica.py
@@ -17,10 +17,8 @@ if __name__ == '__main__':
     SiOd = 1.7  # generous bond distance between Si and O
     ntw.set_connections(SiOd, box=u.dimensions[0:3])
     ntw.assign_oxygens(box=u.dimensions[0:3])
-    #q_SIO = 1.113796741791171  # Renders neutral silica system
-    #q_SIO = 1.1144720182324641  # Renders neutral silica+protein+water system
-    #ntw.add_node_property('charge', {'SIO': q_SIO, 'OSI': -0.55, 'OA': -0.90})
-    ntw.add_node_property('charge', {'SIO': 1.1, 'OSI': -0.55, 'OA': -0.55})
+    q_SIO = 1.1 # Renders neutral silica+protein+water system
+    ntw.add_node_property('charge', {'SIO': q_SIO, 'OSI': -0.55, 'OA': -0.90})
     ntw.add_node_property('mass', {'SIO': 28.0855, 'OSI': 15.9994, 'OA': 15.9994})
     bonds = {('SIO', 'OSI'): {'r': 0.157833041748, 'k': 251040.0},
              ('SIO',  'OA'): {'r': 0.157833041748, 'k': 251040.0},
@@ -30,7 +28,6 @@ if __name__ == '__main__':
               ('SIO', 'OSI', 'SIO'): {'theta': 146.498743845, 'k': 397.480},
               ( 'OA', 'SIO',  'OA'): {'theta': 109.149737561, 'k': 397.480},
             }
-    open('junk','w').write(ntw.header() + ntw.defaults() + ntw.moleculetype() + ntw.atoms() + ntw.bonds(bonds))
-    open('SiO2carved_ovl2.5.itp', 'w').write(ntw.topology(bonds, angles))
+    open('SiO2carved_q0.90_ovl2.5.itp', 'w').write(ntw.topology(bonds, angles))
 
 
